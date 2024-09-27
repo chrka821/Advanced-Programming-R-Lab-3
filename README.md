082bc6d810ce57520e5b166fcbfbc975b7ab1f5f
Christian Kammerer <chrka821@student.liu.se>
2024-09-27 10:59:51 +0200

    Implemented Euclids Algorithm

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Advanced-Programming-R-Lab-3
 University Assignment
+
+ <!-- badges: start -->
+  [![R-CMD-check](https://github.com/chrka821/Advanced-Programming-R-Lab-3/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/chrka821/Advanced-Programming-R-Lab-3/actions/workflows/R-CMD-check.yaml)
+  <!-- badges: end -->
--- lab3victorchris/euclidean.R
+++ lab3victorchris/euclidean.R
@@ -6,11 +6,12 @@ euclidean <- function(x, y){
     y <- temp
   }
   remainder <- -1 
-  while(remainder != 0){
-    remainder = x%%y
-    print(remainder)
-  }
   
+  while(remainder != 0){ # The operation stops if the remainder is 0
+    remainder = x%%y # Compute the remainder of the division of x and y
+    x <- y 
+    y <- remainder # X turns into y, y turns into the remainder
+  }
+  return(x) # X holds the value of the last non zero remainder and thus is the largest divisor
 }
 
-package.skeleton(name="lab3victorchris", list=c("euclidean"))
